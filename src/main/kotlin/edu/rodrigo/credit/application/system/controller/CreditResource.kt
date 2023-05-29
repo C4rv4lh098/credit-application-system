ce1bbc72e810d2713f4acdb94fab556be9b81625
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2023-05-29 19:09:13 -0400

    Endpoints CreditResource - (findByCreditCode)

--- src/main/kotlin/edu/rodrigo/credit/application/system/controller/CreditResource.kt
+++ src/main/kotlin/edu/rodrigo/credit/application/system/controller/CreditResource.kt
@@ -1,15 +1,18 @@
 package edu.rodrigo.credit.application.system.controller
 
 import edu.rodrigo.credit.application.system.dto.CreditDto
+import edu.rodrigo.credit.application.system.dto.CreditView
 import edu.rodrigo.credit.application.system.dto.CreditViewList
 import edu.rodrigo.credit.application.system.entity.Credit
 import edu.rodrigo.credit.application.system.service.impl.CreditService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 import java.util.stream.Collectors
 
 @RestController
@@ -28,5 +31,12 @@ class CreditResource(
         return this.creditService.findAllByCustomer(customerId).stream()
                 .map { credit: Credit -> CreditViewList(credit)}
                 .collect(Collectors.toList())
+    }
+
+    @GetMapping
+    fun findByCreditCode(@RequestParam(value = "customerId") customerId: Long,
+                         @PathVariable creditCode: UUID): CreditView{
+        val credit: Credit = this.creditService.findByCreditCode(customerId, creditCode)
+        return CreditView(credit)
     }
 }
