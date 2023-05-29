70043daf03953818f49075f13dcb34c94ae9b2b4
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2023-05-29 18:48:11 -0400

    Endpoints CreditResource - (save)

---
+++ src/main/kotlin/edu/rodrigo/credit/application/system/controller/CreditResource.kt
@@ -0,0 +1,21 @@
+package edu.rodrigo.credit.application.system.controller
+
+import edu.rodrigo.credit.application.system.dto.CreditDto
+import edu.rodrigo.credit.application.system.entity.Credit
+import edu.rodrigo.credit.application.system.service.impl.CreditService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/credits")
+class CreditResource(
+        private val creditService: CreditService
+) {
+    @PostMapping
+    fun saveCredit(@RequestBody creditDto: CreditDto): String{
+        val credit: Credit = this.creditService.save(creditDto.toEntity())
+        return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved"
+    }
+}
