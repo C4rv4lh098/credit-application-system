0336e84cbfa163f7aa56e0fc19d66509a5b9e2f0
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2023-05-29 15:09:12 -0400

    Implementação CustomerServece(save, findById, delete)

---
+++ src/main/kotlin/edu/rodrigo/credit/application/system/service/impl/CustomerService.kt
@@ -0,0 +1,26 @@
+package edu.rodrigo.credit.application.system.service.impl
+
+import edu.rodrigo.credit.application.system.entity.Customer
+import edu.rodrigo.credit.application.system.repository.CustomerRepository
+import edu.rodrigo.credit.application.system.service.ICustomerService
+import org.springframework.stereotype.Service
+
+@Service
+class CustomerService(
+        private val customerRepository: CustomerRepository
+) : ICustomerService {
+    override fun save(customer: Customer): Customer =
+            this.customerRepository.save(customer)
+
+    override fun findById(id: Long): Customer {
+        return this.customerRepository.findById(id).orElseThrow {
+            throw RuntimeException("Id $id not found")
+        }
+    }
+
+
+    override fun delete(id: Long) {
+        return this.customerRepository.deleteById(id)
+    }
+
+}
