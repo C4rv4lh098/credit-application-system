dc7deb752417be14dae4f8711e1c4b7d99c20526
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2023-05-29 21:09:48 -0400

    Tratando a Exceção DataAccessException

--- src/main/kotlin/edu/rodrigo/credit/application/system/exception/RestExceptionHandler.kt
+++ src/main/kotlin/edu/rodrigo/credit/application/system/exception/RestExceptionHandler.kt
@@ -1,5 +1,6 @@
 package edu.rodrigo.credit.application.system.exception
 
+import org.springframework.dao.DataAccessException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.FieldError
@@ -28,6 +29,19 @@ class RestExceptionHandler {
                     exception = ex.javaClass.toString(),
                     details = erros
             ), HttpStatus.BAD_REQUEST
+        )
+    }
+
+    @ExceptionHandler(DataAccessException::class)
+    fun handleValidException(ex: DataAccessException): ResponseEntity<ExceptionDetails>{
+        return ResponseEntity(
+                ExceptionDetails(
+                        title = "Conflict! Consult the documentation",
+                        timestamp = LocalDateTime.now(),
+                        status = HttpStatus.CONFLICT.value(),
+                        exception = ex.javaClass.toString(),
+                        details = mutableMapOf(ex.cause.toString() to ex.message)
+                ), HttpStatus.CONFLICT
         )
     }
 }
